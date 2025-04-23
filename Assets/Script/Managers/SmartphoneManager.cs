b10c681976365224853bf726a8da4ea4d542f1ef
alto <uzukialto@gmail.com>
2025-04-23 14:03:23 +0900

    スマホ対応

---
+++ Assets/Script/Managers/SmartphoneManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmartphoneManager : MonoBehaviour
+{
+    public bool isSmartphone;
+    void Start()
+    {
+        isSmartphone = UnityEngine.Application.isMobilePlatform;
+        if(!isSmartphone)
+        {
+            gameObject.SetActive(false);
+        }
+
+        Screen.autorotateToLandscapeLeft = true;
+        Screen.autorotateToLandscapeRight = true;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
