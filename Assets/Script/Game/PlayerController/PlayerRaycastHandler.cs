ffe2bbc09506b6f4c4c45aa399a609474e825865
alto <uzukialto@gmail.com>
2026-03-14 00:46:45 +0900

    [add] Gameのスクリプトファイル作成

---
+++ Assets/Script/Game/PlayerController/PlayerRaycastHandler.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game
+{
+    /// <summary>
+    /// プレイヤーとブロックの間のRayを処理するクラス
+    /// </summary>
+    public class PlayerRaycastHandler : MonoBehaviour
+    {
+        void Start()
+        {
+            
+        }
+
+        void Update()
+        {
+            
+        }
+    }
+}
