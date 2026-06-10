5be04a1187adc5d9e8f09be21d8d03459787a899
alto <uzukialto@gmail.com>
2026-06-10 13:47:16 +0900

    [add] Panel検知

--- Assets/Script/Game/PlayerController/PlayerRaycastHandler.cs
+++ Assets/Script/Game/PlayerController/PlayerRaycastHandler.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using MainSystem;
 
 namespace Game
 {
@@ -9,14 +10,44 @@ namespace Game
     /// </summary>
     public class PlayerRaycastHandler : MonoBehaviour
     {
-        void Start()
+        [SerializeField] 
+        private ScreenBase gameScreen;
+        [SerializeField] 
+        private GameObject centerObj;
+        [SerializeField] 
+        private LayerMask panelLayer;
+        public GameObject hitObject { get; private set; }
+        private GameObject screenCamera;
+        private float rayLength;
+
+        void Awake()
         {
-            
+            screenCamera = gameScreen.screenCamera.gameObject;
         }
 
+        // Update is called once per frame
         void Update()
         {
-            
+            Vector3 subCamePos = screenCamera.transform.position;
+            Vector3 centerObjPos = centerObj.transform.position;
+            rayLength = Vector3.Distance(subCamePos, centerObjPos);
+
+            Ray ray = new Ray(centerObjPos, subCamePos);
+
+            Debug.DrawRay(ray.origin, ray.direction * rayLength, Color.green);
+
+            RaycastHit hit;
+
+
+            if (Physics.Raycast(ray, out hit, rayLength, panelLayer))
+            {
+                hitObject = hit.collider.gameObject;
+            }
+            else
+            {
+                hitObject = null;
+            }
+        
         }
     }
 }
