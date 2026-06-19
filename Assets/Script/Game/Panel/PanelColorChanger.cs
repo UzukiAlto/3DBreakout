416f804f19f10b5fe044bca82de0232e9ac785c9
alto <uzukialto@gmail.com>
2026-06-19 12:07:04 +0900

    [update]選択中のパネルの色変更

--- Assets/Script/Game/Panel/PanelColorChanger.cs
+++ Assets/Script/Game/Panel/PanelColorChanger.cs
@@ -1,19 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using MainSystem;
 
 namespace Game
 {
-    public class PanelColorChanger : MonoBehaviour
+    public class PanelColorChanger : MonoBehaviour, IInitializable
     {
-        void Start()
+        [SerializeField] private List<Renderer> panelRendererList;
+        [SerializeField] private Renderer defaultRenderer;
+
+        [SerializeField] private Material operatingMaterial;
+        [SerializeField] private Material selectingMaterial;
+        [SerializeField] private Material defaultMaterial;
+        
+        [SerializeField] private PanelObserver panelObserver;
+
+        public void Initialize()
         {
-            
+            foreach (Renderer renderer in panelRendererList)
+            {
+                renderer.material = defaultMaterial;
+            }
+            defaultRenderer.material = operatingMaterial;
         }
 
-        void Update()
+        void OnEnable()
         {
-            
+            panelObserver.OnSelectedPanelChanged += SelectingPanel;
+        }
+        void OnDisable()
+        {
+            panelObserver.OnSelectedPanelChanged -= SelectingPanel;
+        }
+
+        public void SelectingPanel(GameObject selectObj)
+        {
+            Renderer selectingRenderer = selectObj.GetComponent<Renderer>();
+            Renderer operatingRenderer = panelObserver.currentOperatedPanel.GetComponent<Renderer>();
+            foreach (Renderer renderer in panelRendererList)
+            {
+                if(renderer != operatingRenderer)
+                {
+                    renderer.material = defaultMaterial;
+                }
+            }
+            if(selectingRenderer != operatingRenderer)
+            {
+                selectingRenderer.material = selectingMaterial;
+            }
         }
     }
 }
