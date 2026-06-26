b3c8095e5fba1fab62a2dc5a2938742c3e717174
alto <uzukialto@gmail.com>
2026-06-26 19:25:09 +0900

    [add]BlockGenerator追加
    ブロックを生成するクラスを追加
    ゲームスタート時の処理をGameManagerのイベントで管理するように変更

---
+++ Assets/Script/Game/BlockGenerator.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game
+{
+    public class BlockGenerator : MonoBehaviour
+    {
+        [SerializeField] private GameManager gameManager;
+        [SerializeField] private GameObject cubes;
+        [SerializeField] private GameObject spheres;
+        [SerializeField] private GameObject pyramids;
+        [SerializeField] private GameObject blockParent;
+        [SerializeField] private Material blockMaterial;
+        private List<GameObject> currentBlockList = new List<GameObject>();
+        private List<GameObject> allBlockList = new List<GameObject>();
+
+        private void Awake()
+        {
+            allBlockList = new List<GameObject>()
+            {
+                cubes, spheres, pyramids
+            };
+        }
+
+        private void OnEnable()
+        {
+            gameManager.OnGameStarted += GenerateBlock;
+        }
+
+        void OnDisable()
+        {
+            
+            gameManager.OnGameStarted -= GenerateBlock;
+        }
+
+        public void GenerateBlock()
+        {
+            GameObject nextBlockPrefab = allBlockList[Random.Range(0, allBlockList.Count)];
+            
+            Quaternion quaternion = Quaternion.Euler(Random.Range(-30, 30), Random.Range(-30, 30), 0);
+            GameObject nextBlock = Instantiate(nextBlockPrefab, Vector3.zero, quaternion);
+            nextBlock.transform.SetParent(blockParent.transform);
+            currentBlockList = new List<GameObject>();
+            foreach (Transform childrenBlockTransform in nextBlock.transform)
+            {
+                currentBlockList.Add(childrenBlockTransform.gameObject);
+            }
+        }
+    }
+}
