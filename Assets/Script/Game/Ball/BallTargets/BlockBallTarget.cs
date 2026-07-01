b6ec3f1fc08044947b43fb24480aa67712096a1a
alto <uzukialto@gmail.com>
2026-07-01 18:04:18 +0900

    [add]ブロック削除処理

--- Assets/Script/Game/Ball/BallTargets/BlockBallTarget.cs
+++ Assets/Script/Game/Ball/BallTargets/BlockBallTarget.cs
@@ -6,12 +6,18 @@ namespace Game
 {
     public class BlockBallTarget : MonoBehaviour, IBallTarget
     {
+        private BlockManager blockManager;
         private Color hitBlockColor = new Color(0.89f, 0.91f, 0.40f, 1f);
+        private void Awake()
+        {
+            // BlockはPrefabでありInspectorで設定できないため、GameObject.FindでBlockManagerを取得する
+            blockManager = GameObject.Find("BlockManager").GetComponent<BlockManager>();
+        }
 
         // Ballが衝突した際の反発の仕方を定義
         public void OnHitBallObject(Rigidbody rigidbody, float speed)
         {
-            
+            blockManager.RemoveBlock(this.gameObject);
         }
 
         public void OnHitBallRaycast(Material material, float alpha)
--- Assets/Script/Game/Block/BlockGenerator.cs
+++ Assets/Script/Game/Block/BlockGenerator.cs
@@ -6,13 +6,11 @@ namespace Game
 {
     public class BlockGenerator : MonoBehaviour
     {
-        [SerializeField] private GameManager gameManager;
         [SerializeField] private GameObject cubes;
         [SerializeField] private GameObject spheres;
         [SerializeField] private GameObject pyramids;
         [SerializeField] private GameObject blockParent;
         [SerializeField] private Material blockMaterial;
-        private List<GameObject> currentBlockList = new List<GameObject>();
         private List<GameObject> allBlockList = new List<GameObject>();
 
         private void Awake()
@@ -22,19 +20,7 @@ namespace Game
                 cubes, spheres, pyramids
             };
         }
-
-        private void OnEnable()
-        {
-            gameManager.OnGameReady += GenerateBlock;
-        }
-
-        void OnDisable()
-        {
-            
-            gameManager.OnGameReady -= GenerateBlock;
-        }
-
-        public void GenerateBlock()
+        public GameObject GenerateBlock()
         {
             // 出現させるブロックの種類、傾きをランダムにする
             GameObject nextBlockPrefab = allBlockList[Random.Range(0, allBlockList.Count)];
@@ -42,11 +28,7 @@ namespace Game
             Quaternion quaternion = Quaternion.Euler(Random.Range(-30, 30), Random.Range(-30, 30), 0);
             GameObject nextBlock = Instantiate(nextBlockPrefab, Vector3.zero, quaternion);
             nextBlock.transform.SetParent(blockParent.transform);
-            currentBlockList = new List<GameObject>();
-            foreach (Transform childrenBlockTransform in nextBlock.transform)
-            {
-                currentBlockList.Add(childrenBlockTransform.gameObject);
-            }
+            return nextBlock;
         }
     }
 }
