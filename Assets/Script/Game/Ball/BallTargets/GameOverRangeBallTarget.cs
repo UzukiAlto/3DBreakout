f16ef8c0fa22419e9e4fe8ca4962453b3a3d7a98
alto <uzukialto@gmail.com>
2026-07-30 21:04:33 +0900

    [feat] 残機減少処理

--- Assets/Script/Game/Ball/BallTargets/GameOverRangeBallTarget.cs
+++ Assets/Script/Game/Ball/BallTargets/GameOverRangeBallTarget.cs
@@ -7,11 +7,17 @@ namespace Game
     public class GameOverRangeBallTarget : MonoBehaviour, IBallTarget
     {
         private Color hitGameOverRangeColor = new Color(0.69f, 0.40f, 0.91f, 1f);
+        private GameManager gameManager;
+
+        private void Awake()
+        {
+            gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        }
 
         // Ballが衝突した際の反発の仕方を定義
         public void OnHitBallObject(Rigidbody rigidbody, float speed)
         {
-            
+            gameManager.FailGame();
         }
 
         public void OnHitBallRaycast(Material material, float alpha)
