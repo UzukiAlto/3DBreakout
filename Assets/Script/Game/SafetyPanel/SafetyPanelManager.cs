a991fb536610c0dcea4ca2cdb68fc29291c3ff51
alto <uzukialto@gmail.com>
2026-08-28 23:09:52 +0900

    [add] セーフティパネル追加

---
+++ Assets/Script/Game/SafetyPanel/SafetyPanelManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game
+{
+    public class SafetyPanelManager : MonoBehaviour
+    {
+        void Start()
+        {
+            
+        }
+
+        void Update()
+        {
+            
+        }
+    }
+}
