bc2d007d79fbd6ffaef5c859f75a887c9109aeb7
alto <uzukialto@gmail.com>
2026-08-28 23:09:52 +0900

    [add] セーフティパネル追加

---
+++ Assets/Script/Game/Ball/BallTargets/SafetyPanelBallTarget.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game
+{
+    public class SafetyPanelBallTarget : MonoBehaviour, IBallTarget
+    {
+        
+        public void OnHitBallObject(Rigidbody rigidbody, float ballSpeed)
+        {
+            
+        }
+        public void OnHitBallRaycast(Material material, float alpha)
+        {
+            
+        }
+    }
+}
---
+++ Assets/Script/Game/SafetyPanel/SafetyPanel.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game
+{
+    public class SafetyPanel : MonoBehaviour
+    {
+        [SerializeField] private GameManager gameManager;
+        void Start()
+        {
+            
+        }
+
+        void Update()
+        {
+            
+        }
+    }
+}
